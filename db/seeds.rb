456d350541fa3ddb77c29ee54a29ec3d772e3f94
Suhan Wijaya <suhanw@gmail.com>
2017-10-27 13:35:38 -0400

    fixed seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Route.create!(user_id: 7, name: 'test1', polyline: 'axvwFntpbMkC~I|a@bYtGuR');
-Route.create!(user_id: 7, name: 'test2', polyline: 'yrvwFxisbMvEkOzRjOfNce@vT`R');
-Route.create!(user_id: 7, name: 'test3', polyline: 'oorwFvlsbMdImWnEhC{Nra@`EbE');
-Route.create!(user_id: 7, name: 'test4', polyline: 'axvwFntpbMkC~I|a@bYtGuR');
-Route.create!(user_id: 7, name: 'test5', polyline: 'w_rwFp}zbM~UuqClW}b@');
+Route.create!(user_id: User.find_by(email:'eh@mi.com').id, name: 'test1', polyline: 'axvwFntpbMkC~I|a@bYtGuR');
+Route.create!(user_id: User.find_by(email:'eh@mi.com').id, name: 'test2', polyline: 'yrvwFxisbMvEkOzRjOfNce@vT`R');
+Route.create!(user_id: User.find_by(email:'eh@mi.com').id, name: 'test3', polyline: 'oorwFvlsbMdImWnEhC{Nra@`EbE');
+Route.create!(user_id: User.find_by(email:'eh@mi.com').id, name: 'test4', polyline: 'axvwFntpbMkC~I|a@bYtGuR');
+Route.create!(user_id: User.find_by(email:'eh@mi.com').id, name: 'test5', polyline: 'w_rwFp}zbM~UuqClW}b@');
