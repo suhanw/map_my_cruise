0bfae3197c8d6395aa31f71ebaca9d7a8f8a29ca
Suhan Wijaya <suhanw@gmail.com>
2017-10-31 21:25:26 -0400

    clean up

--- frontend/components/routes/route_index.jsx
+++ frontend/components/routes/route_index.jsx
@@ -67,7 +67,7 @@ class RouteIndex extends React.Component {
           <button className="route-search-button">SEARCH</button>
           <button className="route-search-reset">Reset</button>
           <select className="route-sort-options">
-            <option>Most Recent</option>
+            <option>Coming soon</option>
             <option>Oldest</option>
             <option>Longest</option>
             <option>Shortest</option>
