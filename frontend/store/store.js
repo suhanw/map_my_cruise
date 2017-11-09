009b3cb4ab913e77abd4308fd55a643a61b5bb8f
Suhan Wijaya <suhanw@gmail.com>
2017-11-08 20:18:02 -0500

    fix react minifacication msg

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,13 +1,19 @@
 import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
-
 import RootReducer from '../reducers/root_reducer';
+
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV !== 'production') {
+  const { logger } = require('redux-logger');
+  middlewares.push(logger);
+}
 
 const configureStore = (preloadedState={}) => {
   return createStore(
     RootReducer,
     preloadedState,
-    applyMiddleware(thunk));
+    applyMiddleware(...middlewares));
 };
 
 export default configureStore;
