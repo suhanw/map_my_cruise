ef9366a182bf9eab8597a33336ff45d5ec6e842b
Suhan Wijaya <suhanw@gmail.com>
2017-12-18 17:13:03 -0500

    add zipcode col to users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,7 @@
 #  image_content_type :string
 #  image_file_size    :integer
 #  image_updated_at   :datetime
+#  zipcode            :integer
 #
 
 class User < ApplicationRecord
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171205152043) do
+ActiveRecord::Schema.define(version: 20171218220602) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,6 +81,7 @@ ActiveRecord::Schema.define(version: 20171205152043) do
     t.string "image_content_type"
     t.integer "image_file_size"
     t.datetime "image_updated_at"
+    t.integer "zipcode"
     t.index ["session_token"], name: "index_users_on_session_token"
   end
 
