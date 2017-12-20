8b0be7804608a865dcfa8fcb2a9d5313862e041e
Suhan Wijaya <suhanw@gmail.com>
2017-12-19 21:16:29 -0500

    add polymorphic assoc betw workout and likes

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -22,6 +22,7 @@ class Workout < ApplicationRecord
   belongs_to :user
   belongs_to :route
   has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
 
   has_one :activity, as: :feedable, dependent: :destroy
 
