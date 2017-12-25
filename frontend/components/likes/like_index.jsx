ff2026a31e7b2bba81448e903ac7c9cdf7347823
Suhan Wijaya <suhanw@gmail.com>
2017-12-25 13:05:23 -0500

    create like index container

--- frontend/components/likes/like_index.jsx
+++ frontend/components/likes/like_index.jsx
@@ -1,5 +1,25 @@
 import React from 'react';
+import {connect} from 'react-redux';
 import Spinner from '../spinner';
+// import {createLike, deleteLike} from '../../actions/likes_actions';
+
+const mapStateToProps = (state, ownProps) => {
+  const {session: {currentUser}, entities: {users}} = state;
+  const workoutId = ownProps.workoutId;
+  return {
+    currentUser,
+    users,
+    workoutId,
+  };
+};
+
+const mapDispatchToProps = (state) => {
+  return null;
+  // return {
+  //   createLike: () => dispatch(createLike()),
+  //   deleteLike: (likeId) => dispatch(deleteLike(likeId)),
+  // };
+};
 
 class LikeIndex extends React.Component {
   render() {
@@ -11,4 +31,6 @@ class LikeIndex extends React.Component {
   }
 }
 
-export default LikeIndex;
+export default connect
+  (mapStateToProps, mapDispatchToProps)
+  (LikeIndex);
