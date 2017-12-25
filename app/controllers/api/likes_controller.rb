3fec21bc1dc6edef16c5e5fa397c7e9112237408
Suhan Wijaya <suhanw@gmail.com>
2017-12-25 11:09:09 -0500

    update likes controller

--- app/controllers/api/likes_controller.rb
+++ app/controllers/api/likes_controller.rb
@@ -2,21 +2,20 @@ class Api::LikesController < ApplicationController
   before_action :require_login, :load_likable
 
   def show
-    @comment = Comment.find_by(id: params[:id])
+    @like = Like.find_by(id: params[:id])
   end
 
   def create
     @like = @likable.likes.new(user: current_user)
     if @like.save
-      # to update
-      render json: ['Success!']
+      render json: ['Success!'] #to update
     else
       render json: @like.errors.full_messages, status: 422
     end
   end
 
   def destroy
-
+    
   end
 
   private
