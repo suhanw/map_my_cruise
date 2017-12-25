d2b21838f9c3901f73b9f5c9cdd653119abb2490
Suhan Wijaya <suhanw@gmail.com>
2017-12-25 12:11:40 -0500

    add likes destroy controller

--- app/controllers/api/likes_controller.rb
+++ app/controllers/api/likes_controller.rb
@@ -8,14 +8,20 @@ class Api::LikesController < ApplicationController
   def create
     @like = @likable.likes.new(user: current_user)
     if @like.save
-      render json: ['Success!'] #to update
+      render json: ['Liked!'], status: 200 #to update
     else
       render json: @like.errors.full_messages, status: 422
     end
   end
 
   def destroy
-    
+    @like = Like.find_by(id: params[:id])
+    if !@like
+      render json: ["This like does not exist"], status: 404
+    else
+      @like.destroy
+      render json: ['Unliked!'], status: 200
+    end
   end
 
   private
@@ -23,5 +29,4 @@ class Api::LikesController < ApplicationController
     resource, id = request.path.split('/').slice(2, 2)
     @likable = resource.singularize.classify.constantize.find(id)
   end
-
 end
