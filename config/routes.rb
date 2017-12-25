b448fe2fa5a70bdad54a57ceb2d31f01552cbb87
Suhan Wijaya <suhanw@gmail.com>
2017-12-25 12:26:33 -0500

    make likes in routes.rb polymorphic

--- config/routes.rb
+++ config/routes.rb
@@ -6,22 +6,31 @@ Rails.application.routes.draw do
     resources :users, only: [:create, :update, :index]
     resource :session, only: [:create, :destroy]
 
-    resources :routes, only: [:index, :show, :create, :update, :destroy]
-    resources :workouts, only: [:index, :show, :create, :update, :destroy]
-
-    resources :workouts do
+    concern :commentable do
       resources :comments, only: [:create]
+    end
+
+    concern :likable do
       resources :likes, only: [:create]
     end
 
-    resources :comments, only: [:destroy, :show]
-    resources :likes, only: [:destroy, :show]
+    resources :routes, only: [:index, :show, :create, :update, :destroy]
+    resources :workouts, only: [:index, :show, :create, :update, :destroy], concerns: [:likable, :commentable]
 
     resources :friend_statuses, only: [:index, :create, :update, :destroy, :show]
 
     resources :activities, only: [:index]
 
     resources :notifications, only: [:index, :update]
+
+    # resources :workouts do
+    #   resources :comments, only: [:create]
+    #   # resources :likes, only: [:create]
+    # end
+
+    resources :comments, only: [:destroy, :show]
+    resources :likes, only: [:destroy, :show]
+
 
   end
 
